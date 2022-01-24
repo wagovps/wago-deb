e6d4858a1aa59be62a65b16644d55104a372bbd2
Warren <49498862+wagovps@users.noreply.github.com>
2022-01-25 03:58:24 +0800

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -7,7 +7,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = "127.0.0.1:WsConnectPort"
-RESPONSE = 'WsResponse'
+RESPONSE = 'HTTP/1.1 101 <font color=\"green\">WaGo SSH Debian</font>\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
